87f9879b9689321bd18beab3797387f4c4cdf813
RaresCristescu <49509631+RaresCristescu@users.noreply.github.com>
2022-07-22 10:30:42 +0300

    Add files via upload

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,48 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 7/22/18.
+ */
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+	
+	@RequestMapping({"/", "/index", "/index.html"})
+    public String listOwners(){
+
+
+        return "owners/index";
+    }
+	
+	
+//    private final VetService vetService;
+//
+//    public VetController(VetService vetService) {
+//        this.vetService = vetService;
+//    }
+//
+//    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+//    public String listVets(Model model){
+//
+//        model.addAttribute("vets", vetService.findAll());
+//
+//        return "vets/index";
+//    }
+//
+//    @GetMapping("/api/vets")
+//    public @ResponseBody Set<Vet> getVetsJson(){
+//
+//        return vetService.findAll();
+//    }
+}
